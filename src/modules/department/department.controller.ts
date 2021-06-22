e9ed79b8a0bb9e71fbe5f6a928e3a1b8a4e51acf
Daniel Garcia <daniel.garcia7913@gmail.com>
2021-06-21 20:38:02 -0400

    [ADD]: update_description

--- src/modules/department/department.controller.ts
+++ src/modules/department/department.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { ReadDepartmentDto } from './dtos';
 import { DepartmentService } from './department.service';
+import { Param } from '@nestjs/common';
 
 @Controller('department')
 export class DepartmentController {
@@ -15,5 +16,10 @@ export class DepartmentController {
     @Post()
     createDepartment(@Body() department): Promise<ReadDepartmentDto> {
         return this._departmentService.createDepartment(department);
+    }
+
+    @Patch('/:id')
+    updateDescription(@Param('id',ParseIntPipe) id: number, @Body() description ): Promise<Boolean> {
+        return this._departmentService.updateDescription(id, description);
     }
 }
--- src/modules/department/department.service.ts
+++ src/modules/department/department.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { Department } from './department.entity';
 import { DepartmentRepository } from './department.repository';
@@ -19,5 +19,25 @@ export class DepartmentService {
     async allDepartments(): Promise<ReadDepartmentDto[]> {
         const departments: Department[] = await this._departmentRepository.find();
         return plainToClass(ReadDepartmentDto, departments);
+    }
+
+    async updateDescription( departmentId: number, description: Partial<DepartmentDto> ): Promise<Boolean> {
+        if(!departmentId){
+            throw new BadRequestException('ID does not exist');
+        }
+
+        if(!description){
+            throw new BadRequestException('Description was not given');
+        }
+
+        const updatedDepartment = await this._departmentRepository.update(departmentId,{
+            description: description.description
+        });
+
+        if(!updatedDepartment){
+            throw new NotFoundException('No department was found');
+        }
+
+        return true;
     }
 }
